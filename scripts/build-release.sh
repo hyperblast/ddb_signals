7f08d832e2c555ae57bb360714989100a901ce6f
Hyperblast <hypercode@zoho.com>
2017-08-09 13:16:58 +0300

    add script for building release packages

---
+++ scripts/build-release.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -e
+
+if [ "$VERBOSE" != "" ]; then
+    set -v
+fi
+
+cd "$(dirname $0)/.."
+
+function build {
+    CFLAGS="-O2 -Werror $CFLAGS_EXTRA" make clean pkg
+    mkdir -p build/release
+    cp build/pkg/*.tar.gz build/release
+}
+
+CFLAGS_EXTRA= build
+CFLAGS_EXTRA=-m32 build
