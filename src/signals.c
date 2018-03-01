278835d6f8b913911b8454e1105d12a4124b83f3
Hyperblast <hypercode@zoho.com>
2018-03-02 00:33:22 +0300

    remove license formatting

--- src/signals.c
+++ src/signals.c
@@ -24,22 +24,22 @@
 #define LICENSE_TEXT \
     "Copyright 2016-2017 Hyperblast\n" \
     "\n" \
-    "Permission is hereby granted, free of charge, to any person obtaining a copy\n" \
-    "of this software and associated documentation files (the \"Software\"), to deal\n" \
-    "in the Software without restriction, including without limitation the rights\n" \
-    "to use, copy, modify, merge, publish, distribute, sublicense, and/or sell\n" \
-    "copies of the Software, and to permit persons to whom the Software is\n" \
+    "Permission is hereby granted, free of charge, to any person obtaining a copy " \
+    "of this software and associated documentation files (the \"Software\"), to deal " \
+    "in the Software without restriction, including without limitation the rights " \
+    "to use, copy, modify, merge, publish, distribute, sublicense, and/or sell " \
+    "copies of the Software, and to permit persons to whom the Software is " \
     "furnished to do so, subject to the following conditions:\n" \
     "\n" \
-    "The above copyright notice and this permission notice shall be included in\n" \
+    "The above copyright notice and this permission notice shall be included in " \
     "all copies or substantial portions of the Software.\n" \
     "\n" \
-    "THE SOFTWARE IS PROVIDED \"AS IS\", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR\n" \
-    "IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,\n" \
-    "FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE\n" \
-    "AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER\n" \
-    "LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,\n" \
-    "OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN\n" \
+    "THE SOFTWARE IS PROVIDED \"AS IS\", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR " \
+    "IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, " \
+    "FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE " \
+    "AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER " \
+    "LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, " \
+    "OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN " \
     "THE SOFTWARE.\n"
 
 #define atomic_or(ptr, val) __sync_fetch_and_or((ptr), (val))
